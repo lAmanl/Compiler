2b7314e8b2df6a6ba14217d82d41beecca540c9d
Jai <jai.sharma2023a@gmail.com>
2023-05-14 18:42:31 +0530

    Lab2 2.c

--- Lab2_21BCE5769/2.c
+++ Lab2_21BCE5769/2.c
@@ -1,34 +1,48 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(){
-    char s[5][10] = {"int","float","char","enum","double"}, a[10];
-    int flag = 0;
-    printf("Enter the Identifer/Keyword: " );
-    scanf("%s", a);
-    for(int i = 0; i<5; i++){
-    	if(strcmp(s[i], a) == 0){
-    	    flag = 1;
-    	}
+    FILE *fp = fopen("input.txt", "r");
+    char c = fgetc(fp);
+    int n = (int) c - '0';
+	c = fgetc(fp);
+    char s[5][10] = {"int","float","char","enum","double"};
+    int i;
+    char * w[n];
+
+    for (int i =0; i < n; ++i) {
+    	w[i] = malloc (128); 
+    	fscanf (fp, "%127s", w[i]);
     }
-    if(flag == 1)
-    	printf("It is a Keyword\n");
-    else{
-    	int flag1 = 0, flag2 = 0;
-    	if((a[0] >= 'a' && a[0] <= 'z') || (a[0] >= 'A' && a[0] <= 'Z') || (a[0] == '_'))
-    	    flag1 = 1;
-    	if(flag1 == 1){
-    	    for(int i = 1; i<strlen(a); i++){
-    	    	if(!((a[i] >= 'a' && a[i] <= 'z') || (a[i] >= 'A' && a[i] <= 'Z') || a[i] == '_' || (a[i] >= '0' && a[i] <= '9'))){
-    	    	    printf("Invalid Identifier1\n");
-    	    	    flag2 = 1;
-    	    	    break;
-    	    	}
-    	    }
-    	    if(flag2 == 0)
-    	    	printf("Valid Identifier\n");
-    	}
-    	else
-    	    printf("Invalid Identifier2\n");
-    }
+
+	for(int q = 0; q<n; q++){
+		int flag = 0;
+		// printf("%s", w[q]);
+		for(int i = 0; i<5; i++){
+			if(strcmp(s[i], w[q]) == 0){
+				flag = 1;
+			}
+		}
+		if(flag == 1)
+			printf("%s is a Keyword\n", w[q]);
+		else{
+			int flag1 = 0, flag2 = 0;
+			if((w[q][0] >= 'a' && w[q][0] <= 'z') || (w[q][0] >= 'A' && w[q][0] <= 'Z') || (w[q][0] == '_'))
+				flag1 = 1;
+			if(flag1 == 1){
+				for(int i = 1; i<strlen(w[q]); i++){
+					if(!((w[q][i] >= 'a' && w[q][i] <= 'z') || (w[q][i] >= 'A' && w[q][i] <= 'Z') || w[q][i] == '_' || (w[q][i] >= '0' && w[q][i] <= '9'))){
+						printf("%s is a Invalid Identifier2\n", w[q]);
+						flag2 = 1;
+						break;
+					}
+				}
+				if(flag2 == 0)
+					printf("%s is a Valid Identifier\n", w[q]);
+			}
+			else
+				printf("%s is a Invalid Identifier1\n", w[q]);
+		}
+	}
 }
--- Lab2_21BCE5769/first.c
+++ Lab2_21BCE5769/first.c
@@ -1,9 +1,29 @@
-//This is a comment line that should be removed by processor
-int printf(const char* restrict format,...);
-#define MAX 5
-int main(     ){
-int a =           25	;
-a = 	a+	MAX;
-printf("%d\n",	a);
-return 0;
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+int main(){
+    FILE *fp = fopen("input.txt", "r");
+    char c;
+    c = fgetc(fp);
+    int n = (int)c -'0';
+    int i;
+    char * w[4];
+    //FILE *fp = fopen ("my.txt", "r");
+
+    for (i =0; i < n; ++i) {
+    	w[i] = malloc (128); 
+    	fscanf (fp, "%127s", w[i]);
+    }
+
+    for (i =0; i < n; ++i)
+        printf ("%s\n",w[i]);
+    //do{
+      //  if(c == ' ' || c == '$'){
+            
+        //}
+        //l+=c; 
+	//c = fgetc(fp);
+    //}while(c != EOF);        
+    //for(int i = 0; i<n-1; i++)
+    	//printf("%s", a);
 }
